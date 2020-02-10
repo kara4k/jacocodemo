24002269a9fe93502c849c1088f2c2d6453ecdf5
kara4k <kara4k@gmail.com>
2020-02-10 22:21:56 +0300

    yes

---
+++ innerA/src/test/java/InnerClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InnerClassTest {
+
+    @Test
+    public void doSmth() {
+        Assert.assertTrue(true);
+    }
+}
