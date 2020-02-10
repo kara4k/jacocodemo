4d086626dac68c6cd2c2b3363929d9c28153bc90
kara4k <kara4k@gmail.com>
2020-02-10 22:35:11 +0300

    yes yes

--- innerA/src/test/java/InnerClassTest.java
+++ innerA/src/test/java/InnerClassTest.java
@@ -7,6 +7,8 @@ public class InnerClassTest {
 
     @Test
     public void doSmth() {
+        InnerClass innerClass = new InnerClass();
+        innerClass.doSmth();
         Assert.assertTrue(true);
     }
 }
